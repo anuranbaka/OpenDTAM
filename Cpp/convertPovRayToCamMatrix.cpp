aa964803b36e9053d33e6a099e7170134fddf997
Paul Foster <pfost@umich.fakepart.edu>
2014-05-15 14:04:18 -0400

    fix conversion to OCV coord system based (based on matlab experiments)

--- Cpp/convertPovRayToCamMatrix.cpp
+++ Cpp/convertPovRayToCamMatrix.cpp
@@ -1,6 +1,7 @@
 void convertPovRayToCamMatrix(   //Inputs
+                                 Mat_<double>& cam_pos,
                                  Mat_<double>& cam_dir,
-						         Mat_<double>& cam_up
+						         Mat_<double>& cam_up,
 						         double cam_angle,
 						         double w,
 						         double h,
@@ -13,12 +14,19 @@ void convertPovRayToCamMatrix(   //Inputs
     // p_cf=d*cameraMatrix.inv()*p_ci
     // p_cf=p_cf/p_cf(2)
 
+    // convert povray coordinates to right handed coordinate system
+    cam_dir(2)*=-1;
+    cam_up(2)*=-1;
+    cam_pos(2)*=-1;
 
     //povray uses camright for scaling. We want scaling only in the camera matrix, so we recompute cam_right as a cross product
+    // also, the conversion to a right handed system made it so "cam_right" actually points left, so we fix that at the same 
+    // time. This means only dir and up correspond to the same directions in povray space
     Mat_<double> cam_right(3,1);
-    cam_right(0)=cam_dir(1)*cam_up(2)-cam_dir(2)*cam_up(1);
-    cam_right(1)=cam_dir(2)*cam_up(0)-cam_dir(0)*cam_up(2);
-    cam_right(2)=cam_dir(0)*cam_up(1)-cam_dir(1)*cam_up(0);
+    //cross(-cam_up, cam_right)
+    cam_right(0)=-cam_dir(1)*cam_up(2)+cam_dir(2)*cam_up(1);
+    cam_right(1)=-cam_dir(2)*cam_up(0)+cam_dir(0)*cam_up(2);
+    cam_right(2)=-cam_dir(0)*cam_up(1)+cam_dir(1)*cam_up(0);
 
     //camera intrinsics
     double cx,cy,fx,fy;
@@ -32,11 +40,12 @@ void convertPovRayToCamMatrix(   //Inputs
 										    0,0,1);
 
     //takes rotated camera frame to camera frame
+    // the up is negated because the camera's y axis points down
     Mat cam_rot=(Mat_<double>(3,3) <<
-    cam_right.x, cam_up.x, cam_dir.x, 0,
-    cam_right.y, cam_up.y, cam_dir.y, 0,
-    cam_right.z, cam_up.z, cam_dir.z, 0,
-    0, 0, 0, 1).inv();
+    cam_right.x, -cam_up.x, cam_dir.x, 0,
+    cam_right.y, -cam_up.y, cam_dir.y, 0,
+    cam_right.z, -cam_up.z, cam_dir.z, 0,
+    0, 0, 0, 1).t();
 
     //takes world frame to rotated camera frame
     Mat cam_trans=(Mat_<double>(3,3) <<
