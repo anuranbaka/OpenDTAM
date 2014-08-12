162d9e7852e5f6f17e8f7c5949d01e12bf0d274d
Paul Foster <pfost@umich.fakepart.edu>
2014-08-11 23:41:01 -0400

    Fix dependencies

--- Cpp/graphics.hpp
+++ Cpp/graphics.hpp
@@ -2,6 +2,7 @@
 #define GRAPHICS_HPP
 #include <string>
 #include <opencv2/core/mat.hpp>
+#include <utils/ImplThreadLaunch.hpp>
 void pfShow(const std::string name,const cv::Mat& _mat, int defaultscale=0,cv::Vec2d autoscale=cv::Vec2d(0,0));
 void pfWindow(const std::string name,int prop);
 void* guiLoop(void*);
