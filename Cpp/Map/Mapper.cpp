b3bb06589528dbd12e539f019d16b1d529da08cf
Paul Foster <pfost@umich.fakepart.edu>
2014-08-12 00:23:00 -0400

    Cut down to minimal demo, part 2

--- Cpp/Map/Mapper.cpp
+++
@@ -1,8 +0,0 @@
-Free for non-commercial, non-military, and non-critical use unless incorporated in OpenCV. Inherits OpenCV Licence if in OpenCV.
-
-#include "Mapper.h"
-
-
-
-
-
--- Cpp/utils/Parameters.cpp
+++
@@ -1,9 +0,0 @@
-namespace cv::gpu::odm::params{
-extern int N_UCV;
-
-}
-    thetaStart =    10.0;
-    thetaMin   =     1.0;
-    thetaStep  =      .97;
-    epsilon    =       .1;
-    lambda     =       .01;
